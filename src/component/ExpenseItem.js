1107cc52ab8baeae8f41b4d0beb5284b69a12e7d
Prerana Bisen <bisen.prerna31@gmail.com>
2023-02-21 01:26:06 +0530

    add expenses

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './styles/ExpenseItem.css'
+import React from "react";
+import "./styles/ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
-    
-    return (
-        <div className='expense-item'>
-            <div>{props.date.toDateString()}</div>
-            <div className='expense-item_description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item_price'>{props.amount}</div>
-            </div>
-        </div>
-    )
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item_description">
+        <h2>{props.title}</h2>
+        <div className="expense-item_price">{props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
