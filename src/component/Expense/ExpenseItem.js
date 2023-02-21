3c28a0e9ec755b7a990a0250c4d53633d2d073f5
Prerana Bisen <bisen.prerna31@gmail.com>
2023-02-22 02:25:47 +0530

    Module five

--- src/component/Expense/ExpenseItem.js
+++ src/component/Expense/ExpenseItem.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "./styles/ExpenseItem.css";
+import React, { useState } from "react";
+import "../styles/ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item_description">
         <h2>{props.title}</h2>
         <div className="expense-item_price">{props.amount}</div>
